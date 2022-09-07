0c277fd14892223945b3307aa50498420ca3d76f
Kiran Shila <me@kiranshila.com>
2022-09-07 03:50:06 -0700

    Hardcode version, katversion sucks

--- katcp/__init__.py
+++ katcp/__init__.py
@@ -17,13 +17,4 @@ from .sensortree import (AggregateSensorTree, BooleanSensorTree,
 from .server import (AsyncDeviceServer, DeviceLogger, DeviceServer,
                      DeviceServerBase)
 
-# BEGIN VERSION CHECK
-# Get package version when locally imported from repo or via -e develop install
-try:
-    import katversion as _katversion
-except ImportError:
-    import time as _time
-    __version__ = "0.0+unknown.{}".format(_time.strftime('%Y%m%d%H%M'))
-else:
-    __version__ = _katversion.get_version(__path__[0])
-# END VERSION CHECK
+__version__ = "0.9.1"
--- setup.py
+++ setup.py
@@ -50,8 +50,6 @@ setup(
     platforms=["OS Independent"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     keywords="kat kat7 ska MeerKAT",
-    setup_requires=["katversion"],
-    use_katversion=True,
     install_requires=[
         "ply",
         "future",
